79098943c15dcaac846e39c608815f45105feab3
cczcn <641631251@qq.com>
2019-06-23 23:14:50 +0800

    有点问题

---
+++ something/src/CMakeFiles/run.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run.dir/sensor.cpp.o"
+  "CMakeFiles/run.dir/sensorTest.cpp.o"
+  "run.pdb"
+  "run"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
